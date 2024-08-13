985f5afb48d36bbeafa844d8f6baee303ec9c78b
Angry Baker <checheninlive@mail.ru>
2024-08-13 20:16:29 +0300

    more tests

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -5,6 +5,9 @@ test.each([
      "sfs--666gs", true,
      "sfs--666gs1", false,
      "1sfs--666gs", false,
+     "sfs--6666gs", false,
+     "sfs--666gs-", false,
+     "_sfs--666gs", false,
      "sfsчч666gs", false
   ]
 ])("should validate", (string, expected) => {
