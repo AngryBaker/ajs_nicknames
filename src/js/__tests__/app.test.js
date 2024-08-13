883664b869b993f7db0022b7b9213aaf5f444ec2
Angry Baker <checheninlive@mail.ru>
2024-08-13 20:28:28 +0300

    fix tests

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,16 +1,13 @@
 import Validator from '../app';
 
 test.each([
-  [ 
-     "sfs--666gs", true,
-     "sfs--666gs1", false,
-     "1sfs--666gs", false,
-     "sfs--6666gs", false,
-     "sfs--666gs-", false,
-     "_sfs--666gs", false,
-     "sfsчч666gs", false
-  ]
+  ["sfs--666gs", true,],
+  ["sfs--666gs1", false,],
+  ["sfs--6666gs", false,],
+  ["sfs--666gs-", false,],
+  ["sfsчч666gs", false,],
+  ["_sfs--666gs", false,],
 ])("should validate", (string, expected) => {
   const result = Validator.validateUsername(string);
-  expect(result).toEqual(expected);
+  expect(result).toBe(expected);
 });
